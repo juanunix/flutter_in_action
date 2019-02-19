798ef1b100c0a984e10c2ad24ec860d84c998dc1
grafpoo <grafpoo@gmail.com>
2019-02-19 10:53:56 -0500

    fixed directory name issue and removed hello world (no pubspec)

--- pub_get.sh
+++ pub_get.sh
@@ -4,8 +4,7 @@ cd counter_app && flutter packages get && cd .. &&
 cd dart_pos &&  pub get && cd .. &&
 cd dart_pos_end &&  pub get && cd .. &&
 cd e_commerce_complete &&  flutter packages get && cd .. &&
-cd flutter_firebase_complete &&  flutter packages get && cd .. &&
-cd hello_word_dart && pub get && cd .. &&
+cd flutter_firebase_repository &&  flutter packages get && cd .. &&
 cd shared_lib && pub get && pub run build_runner build && cd .. &&
 cd weather_app_complete && flutter pub get && cd .. &&
 cd weather_app_start_chapter_5 && flutter pub get && cd .. &&
